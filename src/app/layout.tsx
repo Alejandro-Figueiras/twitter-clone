efa967afb67f2bd36cec48f12d127c1d84557d23
Alejandro <alejandrofigueirasx@gmail.com>
2024-12-22 11:10:20 -0500

    Cambia el tema de shadcn a dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='es'>
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className} dark`}>{children}</body>
     </html>
   )
 }

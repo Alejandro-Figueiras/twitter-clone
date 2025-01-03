9f27dc79dfdca87b34dc172aab34c61c4186323f
Alejandro <alejandrofigueirasx@gmail.com>
2025-01-03 11:38:59 -0500

    Agrega autoscroll al textarea

--- src/components/PostsWall/post-form.tsx
+++ src/components/PostsWall/post-form.tsx
@@ -1,23 +1,31 @@
+'use client'
 import { Account } from '@prisma/client'
 import React from 'react'
 import AccountAvatar from '../NavigationSidebar/Avatar/account-avatar'
 import { Textarea } from '../ui/textarea'
+import { Button } from '../ui/button'
 
 type PostFormProps = {
   account: Account
 }
 
 const PostForm = ({ account }: PostFormProps) => {
-  // TODO Make this
-  // https://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize
   return (
     <div className='flex w-full gap-4 border-b border-muted px-4 pb-2 pt-4'>
       <AccountAvatar account={account} />
       <div className='flex w-full flex-col'>
         <Textarea
-          className='h-auto w-full resize-none border-none text-lg focus-visible:outline-none focus-visible:ring-0 active:outline-none'
+          className={`custom-scrollbar h-auto max-h-96 w-full resize-none border-none px-0 text-lg focus-visible:outline-none focus-visible:ring-0 active:outline-none`}
+          onInput={(e) => {
+            e.currentTarget.style.height = 'auto'
+            e.currentTarget.style.height = e.currentTarget.scrollHeight + 'px'
+          }}
           placeholder='¡¿Qué está pasando?!'
         />
+        <div className='flex justify-between'>
+          <div></div>
+          <Button className='rounded-full'>Postear</Button>
+        </div>
       </div>
     </div>
   )
